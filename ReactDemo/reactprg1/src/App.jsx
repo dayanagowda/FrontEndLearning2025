39b6c55ad931ca946146f61584b6190150afca48
dayanagowda <rdayanagowda@gmail.com>
2025-10-16 10:53:46 +0530

    till day 6

---
+++ ReactDemo/reactprg1/src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./component/Header";
+
+
+const App=()=>{
+  return(
+    <>
+     <Header/>{" "}
+     <div>
+       <h1>Hello From React</h1>
+       <p>I am Dayana,From Banglore</p>
+     </div>
+   </>
+  );
+}
+export default App;
